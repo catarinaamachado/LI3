5415d2404fc5f809c8f605c0817ff4e8c242f164
catarinaamachado <catarinamachado11@gmail.com>
2018-03-09 11:11:39 +0000

    structs

---
+++ src/struct.c
@@ -0,0 +1,41 @@
+#include <gmodule.h>
+
+
+typedef struct users {
+  char * shortbio;
+  char * username;
+  int reputation;
+  int n_posts;
+  int next_write;       //              if(next_write == 9) next_write = 0 else next_write++;
+  //BUG: problem array ultimos 10 posts
+} * Users;
+
+typedef struct questions {
+  int post_id;
+  int user_id;
+  char * title;
+  char * tag;  //TODO: funcao que separa a string com as todas tags num array de strings
+  int n_answers;
+  int n_answer_votes;
+  GArray * answers;
+} * Questions;
+
+typedef struct answers{
+  int answer_id;
+  int score;
+} * Answers;
+
+typedef struct day{
+  int n_questions;
+  int n_answers;
+  GSList questions;
+  GSList answers;
+} * Day;
+
+typedef struct TCD_community{
+  GArray * users;
+  GHashTable * questions;
+  GArray * day;  //BUG: algoritmo do tempo indices e datas
+} * TCD_community;
+
+//g_array_new (FALSE, FALSE, sizeof(int));
